0f497f3558465606a70784a4f48917c375fa9296
Garrett Jones <garrettojones@gmail.com>
2020-08-15 00:24:36 -0500

    try 2

---
+++ config.js
@@ -0,0 +1,7 @@
+const YOUTUBE_API_KEY = 'AIzaSyCgX3cN3vw6h2RJSbRWBB2cOREe2yjtpJQ'
+const OAUTH_CLIENT_ID = '839069130407-j12tumf4hinebjvqpstvsegmh0oi8j1k.apps.googleusercontent.com'
+module.exports = {
+    YOUTUBE_API_KEY,
+    OAUTH_CLIENT_ID,
+}
+  //
