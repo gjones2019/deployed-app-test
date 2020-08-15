58b03808008224547c87ebf9cc6184cfc2c74959
austinbrown-opspark <austinworb@gmail.com>
2020-08-15 11:12:29 -0500

    Changed gitignore and config.js for heroku

---
+++ config.js
@@ -0,0 +1,9 @@
+const OAUTH_CLIENT_ID = "1026149554561-9k189ri77spejnogk2nkshkbd0vaivgh.apps.googleusercontent.com";
+const YOUTUBE_API_KEY = "AIzaSyAnIDqtMssJwNgo3kFeIRPkQ8GZ9T8cgCU";
+const PORT = 3000;
+
+module.exports = {
+  OAUTH_CLIENT_ID,
+  YOUTUBE_API_KEY,
+  PORT
+};
