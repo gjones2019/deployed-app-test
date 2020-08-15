9426e05f681857298571316eb367f45a4be1c446
austinbrown-opspark <austinworb@gmail.com>
2020-08-15 18:35:00 -0500

    Testing again

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 const OAUTH_CLIENT_ID = "314042252557-0ckq9tej6vdl0mtrfcftila7tth46h22.apps.googleusercontent.com";
+// const OAUTH_CLIENT_ID = "1026149554561-9k189ri77spejnogk2nkshkbd0vaivgh.apps.googleusercontent.com"
 const YOUTUBE_API_KEY = "AIzaSyAnIDqtMssJwNgo3kFeIRPkQ8GZ9T8cgCU";
 const URL = 'http://jukejams.herokuapp.com'
+// const URL = 'http://localhost'
 const PORT = 3000;
 
 module.exports = {
