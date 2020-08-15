2104ff224f6f3a38facd4d4e3b6832d479ec49de
austinbrown-opspark <austinworb@gmail.com>
2020-08-15 18:55:37 -0500

    Changed PORT

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import QueueEntry from './queueEntry.js';
 // import exampleVideoData from '../fakeData.js';
 import GoogleLogin from 'react-google-login';
 import axios from 'axios';
-import { YOUTUBE_API_KEY, OAUTH_CLIENT_ID, PORT, URL } from '../config.js';
+import { YOUTUBE_API_KEY, OAUTH_CLIENT_ID, URL } from '../config.js';
 import { PORT } from '../server/index.js'
 import { Route, BrowserRouter, Link } from 'react-router-dom';
 import $ from 'jquery';
