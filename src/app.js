7dcda492428bd5c7e206d989714f9c7089e10cff
austinbrown-opspark <austinworb@gmail.com>
2020-08-16 17:28:37 -0500

    fkdfkj

--- src/app.js
+++ src/app.js
@@ -281,17 +281,6 @@ class App extends Component {
 
   // sortPlaylist() {
   //   const { userPlaylist } = this.state;
-
-  //   const compare = (a, b) => {
-  //     if (a is less than b by some ordering criterion) {
-  //       return -1;
-  //     }
-  //     if (a is greater than b by the ordering criterion) {
-  //       return 1;
-  //     }
-  //     // a must be equal to b
-  //     return 0;
-  //   }
   //   this.setState({
   //     userPlaylist: userPlaylist.sort()
   //   })
